b8299d7869e2f274d18242635fdc4234252021f1
Eric Song <ericksong91@gmail.com>
2023-09-27 17:56:40 -0400

    Done with assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
-# your code goes here!
+import time
+
+
+def countdown(t):
+    for x in reversed(range(1, t + 1)):
+        print(f"{x} SECOND(S)!")
+
+    print("HAPPY NEW YEAR!")
+
+
+def countdown_with_sleep(t):
+    for x in reversed(range(1, t + 1)):
+        print(f"{x} SECOND(S)!")
+        time.sleep(1)
+
+    print("HAPPY NEW YEAR!")
+
+
+countdown_with_sleep(5)
